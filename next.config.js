860ae6a2d25156bdd451d0bfd3b52a4dddeb5271
Devender Shekhawat <devender.shekhawat0296@gmail.com>
2023-08-30 23:56:36 +0530

    add env vars to next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    env: {
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY
+    }
 }
 
 module.exports = nextConfig
